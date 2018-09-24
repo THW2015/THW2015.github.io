032ccff0257eda5404762a45603833eac4818bb4
THW2015 <593659637@qq.com>
2018-09-24 12:47:07 +0800

    Site updated: 2018-09-24 12:47:04

---
+++ comment.rb
@@ -0,0 +1,28 @@
+username = "THW2015" # GitHub 用户名
+new_token = "b7978767a5a841dbb2cac98a62dedf85c7bf0cb0"  # GitHub Token
+repo_name = "THW2015.github.io" # 存放 issues
+sitemap_url = "/sitemap.xml" # sitemap
+kind = "gitment" # "Gitalk" or "gitment"
+
+require 'open-uri'
+require 'faraday'
+require 'active_support'
+require 'active_support/core_ext'
+require 'sitemap-parser'
+
+sitemap = SitemapParser.new sitemap_url
+urls = sitemap.to_a
+
+conn = Faraday.new(:url => "https://api.github.com/repos/#{username}/#{repo_name}/issues") do |conn|
+  conn.basic_auth(username, token)
+  conn.adapter  Faraday.default_adapter
+end
+
+urls.each_with_index do |url, index|
+  title = open(url).read.scan(/<title>(.*?)<\/title>/).first.first.force_encoding('UTF-8')
+  response = conn.post do |req|
+    req.body = { body: url, labels: [kind, url], title: title }.to_json
+  end
+  puts response.body
+  sleep 15 if index % 20 == 0
+end
